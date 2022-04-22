a4935c25ca9bf7da5ee2ab321e9f1f2a7efc513c
chandrika46 <41043901+chandrika46@users.noreply.github.com>
2022-04-22 11:32:02 +0530

    Delete mycart directory

--- mycart/over-all-total.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { OverAllTotalPipe } from './over-all-total.pipe';
-
-describe('OverAllTotalPipe', () => {
-  it('create an instance', () => {
-    const pipe = new OverAllTotalPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
